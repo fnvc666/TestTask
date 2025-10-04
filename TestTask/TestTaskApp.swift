94c0ef8c522f08442c045411752e1221114bd05b
Pavel Pavel <frainvoc@gmail.com>
2025-10-04 16:43:35 +0200

    chore: init project, .gitignore

---
+++ TestTask/TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskApp.swift
+//  TestTask
+//
+//  Created by Pavel Pavel on 04/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
