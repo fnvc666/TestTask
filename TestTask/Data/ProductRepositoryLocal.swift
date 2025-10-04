e09ec5d6d22f1a22363cab8ade55c071b9e555ea
Pavel Pavel <frainvoc@gmail.com>
2025-10-04 18:10:13 +0200

    feat(data): local ProductsRepository & fill products.json

--- TestTask/Data/ProductRepositoryLocal.swift
+++ TestTask/Data/ProductRepositoryLocal.swift
@@ -5,3 +5,19 @@
 //  Created by Pavel Pavel on 04/10/2025.
 //
 
+import Foundation
+
+final class ProductRepositoryLocal {
+    public init() {}
+    
+    public func fetch() async throws -> [Product] {
+        guard let url = Bundle.main.url(forResource: "products", withExtension: "json") else {
+            throw NSError(domain: "product", code: 1, userInfo: [NSLocalizedDescriptionKey: "products.json not found"])
+        }
+        
+        let data = try Data(contentsOf: url)
+        let decoder = JSONDecoder()
+        
+        return try decoder.decode([Product].self, from: data)
+    }
+}
