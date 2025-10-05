5a9b128f0cdc9ea8cb74d39c8ff96248d82151e9
Pavel Pavel <frainvoc@gmail.com>
2025-10-05 22:38:33 +0200

    feat(browser): add base BrowserViewModel

--- TestTask/Features/Browser/BrowserViewModel.swift
+++ TestTask/Features/Browser/BrowserViewModel.swift
@@ -6,7 +6,28 @@
 //
 
 import SwiftUI
+import WebKit
 
 final class BrowserViewModel: ObservableObject {
-    //
+    @Published var isLoading = false
+    @Published var canBack = false
+    @Published var canFwd = false
+    @Published var progrss: Double = 0
+    
+    var goBack: () -> Void = { }
+    var goFwd: () -> Void = { }
+    var reload: () -> Void = { }
+    var loadStarted: () -> Void = { }
+    
+    let startURL = URL(string: "https://www.google.com")!
+    
+    func back() { goBack() }
+    func forward() { goFwd() }
+    func refresh() { reload() }
+
+    func updateNavAvailability(from wv: WKWebView?) {
+        guard let wv else { canBack = false; canFwd = false; return }
+        canBack = wv.canGoBack
+        canFwd = wv.canGoForward
+    }
 }
